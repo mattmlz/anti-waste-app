d09c0d12b9a912a5ab262ea2d9fa9f3d1703bd39
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2019-04-01 17:34:33 +0200

    Init projet :rocket:

---
+++ anti-waste/anti-waste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anti-waste
+//
+//  Created by Matthieu T on 01/04/2019.
+//  Copyright © 2019 Matthieu TOUSSAINT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
