48f97246acf30bb36eb8036f47feb2cb3b0510ae
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2019-04-02 14:58:41 +0200

    add fictive onboarding screens

--- anti-waste/anti-waste/ViewController.swift
+++ anti-waste/anti-waste/ViewController.swift
@@ -10,24 +10,112 @@ import UIKit
 import SwiftyOnboard
 
 class ViewController: UIViewController {
-
+    var swiftyOnboard: SwiftyOnboard!
+    let colors:[UIColor] = [#colorLiteral(red: 0.9980840087, green: 0.3723873496, blue: 0.4952875376, alpha: 1),#colorLiteral(red: 0.2666860223, green: 0.5116362572, blue: 1, alpha: 1),#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)]
+    var titleArray: [String] = ["Bienvenue dans votre nouvelle application anti-gaspillage !", "Des propositions certifiées", "Rejoignez la communauté anti-gaspillage !"]
+    var subTitleArray: [String] = ["Anti-waste va vous faire découvrir\n comment arrêter de jeter des produits\n dont la date limite de consommation est dépassée ou à quelques\n jours de l'être.", "Toutes les propositions anti-gaspillage\n sont validées par des médecins nutritionnistes et Les Restos du Coeur.", "Qu'attendez-vous pour arrêter de jeter l'argent par les fenêtres\n et faire un geste pour la planète ?"]
+    
+    var gradiant: CAGradientLayer = {
+        //Gradiant for the background view
+        let blue = UIColor(red: 69/255, green: 127/255, blue: 202/255, alpha: 1.0).cgColor
+        let purple = UIColor(red: 166/255, green: 172/255, blue: 236/255, alpha: 1.0).cgColor
+        let gradiant = CAGradientLayer()
+        gradiant.colors = [purple, blue]
+        gradiant.startPoint = CGPoint(x: 0.5, y: 0.18)
+        return gradiant
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        gradient()
         
-        let swiftyOnboard = SwiftyOnboard(frame: view.frame)
+        swiftyOnboard = SwiftyOnboard(frame: view.frame, style: .light)
         view.addSubview(swiftyOnboard)
-        swiftyOnboard.dataSource = self as? SwiftyOnboardDataSource
+        swiftyOnboard.dataSource = self
+        swiftyOnboard.delegate = self
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func gradient() {
+        //Add the gradiant to the view:
+        self.gradiant.frame = view.bounds
+        view.layer.addSublayer(gradiant)
+    }
+    
+    @objc func handleSkip() {
+        swiftyOnboard?.goToPage(index: 2, animated: true)
+    }
+    
+    @objc func handleContinue(sender: UIButton) {
+        let index = sender.tag
+        swiftyOnboard?.goToPage(index: index + 1, animated: true)
     }
 }
 
-extension ViewController: SwiftyOnboardDataSource {
+extension ViewController: SwiftyOnboardDelegate, SwiftyOnboardDataSource {
     
     func swiftyOnboardNumberOfPages(_ swiftyOnboard: SwiftyOnboard) -> Int {
+        //Number of pages in the onboarding:
         return 3
     }
     
-    func swiftyOnboardPageForIndex(_ swiftyOnboard: SwiftyOnboard, index: Int) -> SwiftyOnboardPage? {
-        let page = SwiftyOnboardPage()
-        return page
+    func swiftyOnboardBackgroundColorFor(_ swiftyOnboard: SwiftyOnboard, atIndex index: Int) -> UIColor? {
+        //Return the background color for the page at index:
+        return colors[index]
     }
+    
+    func swiftyOnboardPageForIndex(_ swiftyOnboard: SwiftyOnboard, index: Int) -> SwiftyOnboardPage? {
+        let view = SwiftyOnboardPage()
+        
+        //Set the image on the page:
+        view.imageView.image = UIImage(named: "onboard\(index)")
+        
+        //Set the font and color for the labels:
+        view.title.font = UIFont(name: "Lato-Heavy", size: 22)
+        view.subTitle.font = UIFont(name: "Lato-Regular", size: 16)
+        
+        //Set the text in the page:
+        view.title.text = titleArray[index]
+        view.subTitle.text = subTitleArray[index]
+        
+        //Return the page for the given index:
+        return view
+    }
+    
+    func swiftyOnboardViewForOverlay(_ swiftyOnboard: SwiftyOnboard) -> SwiftyOnboardOverlay? {
+        let overlay = SwiftyOnboardOverlay()
+        
+        //Setup targets for the buttons on the overlay view:
+        overlay.skipButton.addTarget(self, action: #selector(handleSkip), for: .touchUpInside)
+        overlay.continueButton.addTarget(self, action: #selector(handleContinue), for: .touchUpInside)
+        
+        //Setup for the overlay buttons:
+        overlay.continueButton.titleLabel?.font = UIFont(name: "Lato-Bold", size: 16)
+        overlay.continueButton.setTitleColor(UIColor.white, for: .normal)
+        overlay.skipButton.setTitleColor(UIColor.white, for: .normal)
+        overlay.skipButton.titleLabel?.font = UIFont(name: "Lato-Heavy", size: 16)
+        
+        //Return the overlay view:
+        return overlay
+    }
+    
+    func swiftyOnboardOverlayForPosition(_ swiftyOnboard: SwiftyOnboard, overlay: SwiftyOnboardOverlay, for position: Double) {
+        let currentPage = round(position)
+        overlay.pageControl.currentPage = Int(currentPage)
+        print(Int(currentPage))
+        overlay.continueButton.tag = Int(position)
+        
+        if currentPage == 0.0 || currentPage == 1.0 {
+            overlay.continueButton.setTitle("Continuer", for: .normal)
+            overlay.skipButton.setTitle("Passer", for: .normal)
+            overlay.skipButton.isHidden = false
+        } else {
+            overlay.continueButton.setTitle("C'est parti !", for: .normal)
+            overlay.skipButton.isHidden = true
+        }
+    }
+
 }
