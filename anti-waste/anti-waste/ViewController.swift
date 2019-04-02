4d8e6be206c0f2fc7236cdd906a2c76e0478eb1f
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2019-04-02 11:05:06 +0200

    add pod for onboarding screen :heavy_plus_sign:

--- anti-waste/anti-waste/ViewController.swift
+++ anti-waste/anti-waste/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storyboard = UIStoryboard(name: "Scanner", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "ScannerViewController") as! ScannerViewController
+        self.present(controller, animated: true, completion: nil)
     }
-
-
 }
 
